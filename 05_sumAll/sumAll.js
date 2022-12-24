f82a369ba6fc551fcc1fb05df54bccd8008f7523
Gagan Gowda <gagangowda26@gmail.com>
2022-12-24 13:48:42 -0800

    Completed TDD

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const ERROR = 'ERROR'
+const sumAll = function(m, n) {
+    // negative numbers edge case 
+    if (m < 0 || n < 0) {
+        return ERROR
+    }
 
+    // non-number cases
+    if (typeof(m) !== "number" || typeof(n) !== 'number') {
+        return ERROR
+    }
+
+    // mandating a to be the smaller always.
+    a = m <= n ? m : n;
+    b = m > n ? m : n;
+
+    sum = 0
+    for (let i = a; i <= b; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
