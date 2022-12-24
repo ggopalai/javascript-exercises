de58a0c37a3947056e98289511960968db879197
Gagan Gowda <gagangowda26@gmail.com>
2022-12-24 12:53:57 -0800

    First 2 tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(s, n) {
+    if (n < 0) {
+        return 'ERROR'
+    }
+    res = ''
+    for (let i = 0; i < n; i ++) {
+        res += s
+    }
+    return res
 };
 
 // Do not edit below this line
