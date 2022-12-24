98124da1a63a42be53bfc49403159939b6a7e36d
Gagan Gowda <gagangowda26@gmail.com>
2022-12-24 13:19:19 -0800

    Tests 3 and 4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    res = '';
+    for (s of str) {
+        res = s + res
+    }
+    return res
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr) {
+    args = Array.from(arguments)
+    for (i of args) {
+        ind = arr.indexOf(i)
+        if (ind > -1) {
+            arr.splice(ind, 1)
+        }
+    }
+    return arr
 };
 
 // Do not edit below this line
