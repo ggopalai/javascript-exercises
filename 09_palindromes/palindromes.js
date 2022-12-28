691a964b3f233df5c11013dc4e10f04b200c28ba
Gagan Gowda <gagangowda26@gmail.com>
2022-12-28 13:46:36 -0800

    Completed 09 and 12 with some help

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (ns) {
+   const processed = ns.toLowerCase().replace(/[^a-z]/g, "");
+   return (processed.split("").reverse().join("") == processed);
 };
 
 // Do not edit below this line
